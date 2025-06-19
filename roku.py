a9fb0d08735de29605e6914d0b3c0236e7f61f85
Lakshmanan Chelliah <lakshmananmailbag@gmail.com>
2025-06-19 20:54:30 +0530

    Update roku.py

--- roku.py
+++ roku.py
@@ -296,7 +296,7 @@ class Authentication:
        
         with col1:
             st.write("A product of")
-            st.image(https://github.com/clakshmanan/roku_data/contec.png, width=175)
+            st.image("https://github.com/clakshmanan/roku_data/contec.png", width=175)
             st.subheader("ROKU_CONTEC")
             #st.header(datetime.now().strftime("%H:%M"))
             #utc_now = datetime.utcnow().strftime('%Y-%m-%d   %H:%M:%S')
